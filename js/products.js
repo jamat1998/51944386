e9f069ed7152f09e4a35a0d1cc6f09658d218513
Mathias Peyrano <102566698+jamat1998@users.noreply.github.com>
2022-08-10 22:58:19 -0300

    Categoria-Productos

--- js/products.js
+++ js/products.js
@@ -1,17 +1,26 @@
+const d=document;
 async function getData(){
+    let container = d.querySelector('.prod');
+    console.log(container);
     let data = await fetch(PRODUCTS_URL);
     if(data.ok){
         let autos = await data.json(); 
         let info = autos.products;
         for (const auto of info) {
-            parrafo.innerHTML += `
-                <div class="pokemon">
-                    <div>${pokemon.name}</div>
-                    <img src="${imagenDelPokemon.sprites.back_default}">
-                    <img src="${imagenDelPokemon.sprites.front_default}">
-                </div>
+            container.innerHTML += 
+            `    
+            <div class='products_container'>
+            <img class='product-img' src='${auto.image}'>     
+            <div class='grid_products'>
+            <p>Modelo: ${auto.name}</p>
+            <p>Costo: ${auto.currency} ${auto.cost}</p>
+            <p>Descripcion General: ${auto.description}</p>
+            <p>Vendidos: ${auto.soldCount}</p>
+            </div>
+            </div>
             `
         }
   
     }
   }
+  getData();
