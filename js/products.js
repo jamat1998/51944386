ab84b3a679db4443d5f125d224cfaa5c3844c146
Mathias Peyrano <102566698+jamat1998@users.noreply.github.com>
2022-08-24 22:45:14 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,24 +1,103 @@
 
+function inner(product, array){
+    let container = document.querySelector('#cat-list-container');
+    for (product of array) {
+        container.innerHTML += 
+       `    
+       <div class='products_container'>
+       <img class='product-img' src='${product.image}'>     
+       <div class='cards'>
+       <p id='name'>${product.name} - ${product.currency} ${product.cost}</p>
+       <p id='vendidos'>${product.soldCount} Vendidos</p>
+       <p id='description'>${product.description}</p>
+       </div>
+       </div>
+       ` 
+}
+}
 async function getData(){
-    let container = document.querySelector('.prod');
     let data = await fetch(`https://japceibal.github.io/emercado-api/cats_products/${localStorage.getItem('catID')}.json`);
     if(data.ok){
+        let container = document.querySelector('#cat-list-container');
         let autos = await data.json(); 
         let info = autos.products;
-        for (const auto of info) {
-            container.innerHTML += 
-            `    
-            <div class='products_container'>
-            <img class='product-img' src='${auto.image}'>     
-            <div class='cards'>
-            <p id='name'>${auto.name} - ${auto.currency} ${auto.cost}</p>
-            <p id='vendidos'>${auto.soldCount} Vendidos</p>
-            <p id='description'>${auto.description}</p>
-            </div>
-            </div>
-            `
+        inner(autos, info);
+        
+        document.getElementById('sortAsc').addEventListener('click', function(){
+            const asc = info.sort(function(a, b) {
+                let aCount = parseInt(a.cost);
+                let bCount = parseInt(b.cost);
+                if ( aCount < bCount ){ return -1; }
+                if ( aCount > bCount ){ return 1; }
+                return 0;
+            })
+            container.innerHTML = ''
+            inner(autos, asc)
+        })
+        
+        document.getElementById('sortDesc').addEventListener('click', function(){
+            
+            const des = info.sort(function(a, b) {
+                let aCount = parseInt(a.cost);
+                let bCount = parseInt(b.cost);
+                if ( aCount > bCount ){ return -1; }
+                if ( aCount < bCount ){ return 1; }
+                return 0;
+            })
+            container.innerHTML = ''
+            inner(autos, des)
+        })
+        
+        document.getElementById('sortByRel').addEventListener('click', function(){
+            const rel = info.sort(function(a, b) {
+                let aCount = parseInt(a.soldCount);
+                let bCount = parseInt(b.soldCount);
+                if ( aCount > bCount ){ return -1; }
+                if ( aCount < bCount ){ return 1; }
+                return 0;
+            })
+            container.innerHTML = ''
+            inner(autos, info)
+        })    
+        
+        document.getElementById("clearRangeFilter").addEventListener("click", function(){
+            document.getElementById("rangeFilterPriceMin").value = "";
+            document.getElementById("rangeFilterPriceMax").value = "";
+        });
+        
+        document.getElementById("rangeFilterPrice").addEventListener("click", function(){
+            minCount = document.getElementById("rangeFilterPriceMin").value;
+            maxCount = document.getElementById("rangeFilterPriceMax").value;
+            container.innerHTML = ''
+            if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
+                minCount = parseInt(minCount);
+            }
+            else{
+                minCount = undefined;
+            }
+            if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
+                maxCount = parseInt(maxCount);
+            }
+            else{
+                maxCount = undefined;
+            }
+            for (let product of info) {
+                if (((minCount == undefined) || (minCount != undefined && parseInt(product.cost) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(product.cost) <= maxCount))){
+                    container.innerHTML += 
+                   `    
+                   <div class='products_container'>
+                   <img class='product-img' src='${product.image}'>     
+                   <div class='cards'>
+                   <p id='name'>${product.name} - ${product.currency} ${product.cost}</p>
+                   <p id='vendidos'>${product.soldCount} Vendidos</p>
+                   <p id='description'>${product.description}</p>
+                   </div>
+                   </div>
+                   ` 
+                }
         }
-  
+    })
     }
-  }
-  getData();
+}
+        getData();
