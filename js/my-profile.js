648ec9ef8b7dc05a6185abafb59f38a1f9bd9859
jamat1998 <mathias.14.1998@hotmail.com>
2022-10-30 15:54:29 -0300

    :v:

--- js/my-profile.js
+++ js/my-profile.js
@@ -9,23 +9,25 @@ const viewImage = document.getElementById("viewImage");
 const btnSaveChanges = document.getElementById("btnSaveChanges");
 const saveChanges = document.getElementById("saveChanges");
 
+//asigno valor por defecto al campo email.
 let emailValue = localStorage.getItem("emailValue");
 emailInput.value = emailValue;
 
-  imageInput.addEventListener('change',(e)=>{
-    const reader= new FileReader();
-  
-    reader.addEventListener('load',()=>{
-      localStorage.setItem(`imageProfile de ${emailValue}`, reader.result)
-    })
-    
-    reader.readAsDataURL(e.target.files[0])
-  })
-  
+//imagen en 64bits y la guardo en el localstorage
+imageInput.addEventListener("change", (e) => {
+  const reader = new FileReader();
 
+  reader.addEventListener("load", () => {
+    localStorage.setItem(`imageProfile de ${emailValue}`, reader.result);
+  });
+
+  reader.readAsDataURL(e.target.files[0]);
+});
+
+//evento para manipular el formulario
 saveChanges.addEventListener("submit", (e) => {
   e.preventDefault();
-  const imageProfile= localStorage.getItem(`imageProfile de ${emailValue}`)
+  const imageProfile = localStorage.getItem(`imageProfile de ${emailValue}`);
   let res = {
     secondName: secondNameInput.value,
     firstSurname: firstSurnameInput.value,
@@ -33,36 +35,35 @@ saveChanges.addEventListener("submit", (e) => {
     firstName: firstNameInput.value,
     email: emailInput.value,
     secondSurname: secondSurnameInput.value,
-    imageProfile
+    imageProfile,
   };
 
   localStorage.setItem(`${emailValue}`, JSON.stringify(res));
+
+  //alerta de formulario
   btnSaveChanges.innerHTML += 
   `<div id='alertSuccess' class="alert alert-success" role="alert">
-         Informacion guardada satisfactoriamente!
-         </div>`;
-         setTimeout(() => {
-           document.getElementById("alertSuccess").remove();
-           location.reload()
-          }, 2500);
-        
-          
-        });
-        
-        document.addEventListener("DOMContentLoaded", () => {
-          
-          let items = JSON.parse(localStorage.getItem(`${emailValue}`));
-          if(items){
-            secondNameInput.value = items.secondName;
-            firstSurnameInput.value = items.firstSurname;
-            contactInput.value = items.contact;
-            firstNameInput.value = items.firstName;
-            emailInput.value = items.email;
-            secondSurnameInput.value = items.secondSurname;
-            viewImage.setAttribute('src', items.imageProfile)
-          }
-          if(!items){
-            viewImage.setAttribute('src', './img/profile.jpg')
-          }
-          
+      Informacion guardada satisfactoriamente!
+  </div>`;
+  setTimeout(() => {
+    document.getElementById("alertSuccess").remove();
+    location.reload();
+  }, 2500);
+});
+
+//evento para asignarle a los campos la informacion guardada del usuario en el localstorage
+document.addEventListener("DOMContentLoaded", () => {
+  let items = JSON.parse(localStorage.getItem(`${emailValue}`));
+  if (items) {
+    secondNameInput.value = items.secondName;
+    firstSurnameInput.value = items.firstSurname;
+    contactInput.value = items.contact;
+    firstNameInput.value = items.firstName;
+    emailInput.value = items.email;
+    secondSurnameInput.value = items.secondSurname;
+    viewImage.setAttribute("src", items.imageProfile);
+  }
+  if (!items) {
+    viewImage.setAttribute("src", "./img/profile.jpg");
+  }
 });
